3ead9cd56f888b34c327d0d2cd26a968d9d9ac5d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-10-26 19:45:09 +0530

    base: Enable DT2S on lockscreen based on DT2W
    Change-Id: Id106d28ca53de2e2c2b2fd46fd2fdd694e7c0df7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5176,13 +5176,6 @@ public final class Settings {
          */
 
         /**
-         * Double tap on lockscreen to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN =
-                "double_tap_sleep_lockscreen";
-
-        /**
          * Enable statusbar double tap gesture to put device to sleep
          * @hide
          */
@@ -5314,7 +5307,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
-            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
         }
 
